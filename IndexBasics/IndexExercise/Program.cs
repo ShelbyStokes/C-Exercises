ed79b845121cfb6a6d3cfa7e9f854c0e74116ca2
Shelby Stokes <Stokes4505@gmail.com>
2018-11-06 14:09:15 -0800

    Adding PackageExpress and IndexBasics

---
+++ IndexBasics/IndexExercise/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace IndexExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Howdy program
+            string[] stringArray = { "Hello", "Howdy", "Hola", "Chello you" };
+            Console.WriteLine("Enter a number 0 - 3 to generate a greeting.");
+            int greeting = Convert.ToInt32(Console.ReadLine());
+
+            if (greeting < 4)
+            {
+
+                Console.WriteLine(stringArray[greeting]);
+            }
+            else
+            {
+                Console.WriteLine("The submitted number does not fall between  0 and 3");
+            }
+
+
+            //Integer index 
+            int[] intArray = { 0, 10, 100, 1000, 10000, 100000 };
+            Console.WriteLine("\n Enter a number 0 - 5 to generate a number.");
+            int numberdis = Convert.ToInt32(Console.ReadLine());
+
+            if (numberdis < 6)
+            {
+
+                Console.WriteLine(intArray[numberdis]);
+            }
+            else
+            {
+                Console.WriteLine("The submitted number does not fall between  0 and 5");
+            }
+
+            //List of Strings
+            Console.WriteLine("\n List of words on display \n 0. Banana \n 1. Hot air balloon \n 2. dynamite \n 3. Spiderman \n \n Pick you favorite word in this list using 1 - 4:");
+            string[] listArray = { "Banana", "Hot air balloon", "dynamite", "Spiderman" };
+            int wordIn = Convert.ToInt32(Console.ReadLine());
+
+            if (wordIn < 4)
+            {
+
+                Console.WriteLine(listArray[wordIn]);
+            }
+            else
+            {
+                Console.WriteLine("The submitted number is not listed. Goodbye");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine();
+            Console.WriteLine("What is the weight of the package you wish to ship?");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else
+            {
+            Console.WriteLine("What is the packages width?");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the packages height?");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the packages lenght?");
+            int packageLenght = Convert.ToInt32(Console.ReadLine());
+
+                decimal totalDemensions = (packageWidth * packageHeight * packageLenght);
+                //Console.WriteLine(totalDemensions); - used to check math while building program
+
+                if (totalDemensions > 50)
+                {
+                    Console.WriteLine("package too big to be shipped via package express.");
+                }
+                else
+                {
+                    decimal shipquote = (totalDemensions * packageWeight)/100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $"+ shipquote +".00");
+                    Console.WriteLine("Thank you.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
